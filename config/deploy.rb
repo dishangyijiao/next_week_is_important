ef0efcbd4c568c68d53603b7cd6642270f00d8af
zhangxingzhi <zhangxingzhizailushang@gmail.com>
2019-02-26 23:52:23 +0800

    Fix: adjust deploy setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,14 @@
 sh "ssh-add"
 lock "~> 3.10.1"
 
-set :application, "Next-Week-Is-Important"
-set :repo_url, "git@github.com:zhangxingzhizailushang/Next-Week-Is-Important.git"
+set :application, "rails_nextweek"
+set :repo_url, "git@github.com:dishangyijiao/next_week_is_important.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/Next-Week-Is-Important"
+set :deploy_to, "/home/deploy/rails_nextweek"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "47.92.95.165", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "39.98.203.135", user: "deploy", roles: %w{ app db web }, my_property: :my_value
 
 set :branch, "master"
 
